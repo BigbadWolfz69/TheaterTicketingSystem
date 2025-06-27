b6e68ba942adc9d3ecf92fa51857f966c9411c79
BudjetMvFilm <BudjetMvFilm@DESKTOP-2F393AM>
2025-06-27 18:12:48 +0800

    VALIENTE LOGIN FORM UI 06-27-25

---
+++ TheaterTicketingSystem/Class/TextEditPlaceholder.cs
@@ -0,0 +1,18 @@
+﻿using DevExpress.XtraEditors;
+
+/// <summary>
+/// Adds a grey placeholder that vanishes on focus/typing.
+/// </summary>
+
+
+public static class TextEditPlaceholderExtensions
+{
+
+    public static void SetPlaceholder(this TextEdit editor, string prompt)
+    {
+        var p = editor.Properties;
+        p.NullValuePrompt = prompt;
+        p.ShowNullValuePrompt = ShowNullValuePromptOptions.EmptyValue;
+  
+    }
+}
--- TheaterTicketingSystem/Program.cs
+++ TheaterTicketingSystem/Program.cs
@@ -5,6 +5,7 @@ using System.Windows.Forms;
 using DevExpress.LookAndFeel;
 using DevExpress.Skins;
 using DevExpress.UserSkins;
+using TheaterTicketingSystem.Forms;
 
 namespace TheaterTicketingSystem
 {
@@ -18,7 +19,7 @@ namespace TheaterTicketingSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
         }
     }
 }
